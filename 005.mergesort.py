8681a1264c70285104f962c77552bdefea4938f7
garamappa <wel2u@naver.com>
2024-05-04 23:01:46 +0900

    well. I am not sure. but it worked

---
+++ 005.mergesort.py
@@ -0,0 +1,48 @@
+S =[27, 10, 12, 20,25, 13,15,22]
+
+def merge(u,v):
+    i=j = 0
+    s=[]
+
+    while (i < len(u) and j < len(v)):
+        if u[i] > v[j]:
+            s.append(v[j])
+            j += 1
+        else:
+            s.append(u[i])
+            i += 1
+
+    if i < len(u):
+        s += u[i:]
+    else:
+        s += v[j:]
+
+    return s
+
+def mergesort(s):
+    length = len(s)
+
+    if length <= 1:
+        return s
+    
+    mid = length // 2
+
+    u = mergesort(s[0:mid])
+    v = mergesort(s[mid:length])
+
+    return merge(u,v)
+
+
+
+
+
+
+
+
+
+
+print('Before:',S)
+X =mergesort(S)
+print('After:',X)
+
+
