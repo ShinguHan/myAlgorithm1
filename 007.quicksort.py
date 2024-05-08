363d373961cb5c04cc13ad4614611ce441d3179b
garamappa <wel2u@naver.com>
2024-05-08 22:02:42 +0900

    correct

--- 007.quicksort.py
+++ 007.quicksort.py
@@ -2,23 +2,44 @@ S = [4,6,1,43,9,3,26,7,34,2341,234]
 
 # S= [26, 5, 37, 1, 61, 11, 59, 15, 48, 19]
 
-def partition(S, low, high):
-    point = S[low]
-    i,j = low+1,high
+# def partition(S, low, high):
+#     point = S[low]
+#     i,j = low+1,high
 
-    while(i<=j):
-        while(S[i]<point and i<len(S)):
-            i+=1
-        while(S[j]>point):
-            j-=1
-        if(i<j):
-            S[i],S[j]=S[j],S[i]
+#     while(i<=j):
+#         while(S[i]<point):
+#             i+=1
+#         while(S[j]>point):
+#             j-=1
+#         if(i<=j):
+#             S[i],S[j]=S[j],S[i]
+#         else:
+#             break
     
-    pivot = j
+#     pivot = j
 
-    S[low], S[pivot] = S[pivot], S[low]
+#     S[low], S[pivot] = S[pivot], S[low]
 
-    return pivot
+#     return pivot
+
+def partition(S, low, high):
+    pivot = S[low]  # pivot
+    i = low + 1
+    j = high
+
+    while True:
+        while i <= j and S[i] < pivot:
+            i += 1
+        while i <= j and S[j] > pivot:
+            j -= 1
+        if i <= j:
+            S[i], S[j] = S[j], S[i]  # swap
+        else:
+            break
+
+    S[low], S[j] = S[j], S[low]  # swap pivot to its correct position
+    return j
+
 
 def quicksort(S, low, high):
     if high > low:
